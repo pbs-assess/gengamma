6b6ea080e8cddbe84db5967bfc1917f6072558ef
Jillian Dunic <>
2024-04-05 15:05:06 -0700

    good draft of simulation plots

--- R/02-plot-cross-simulation.R
+++ R/02-plot-cross-simulation.R
@@ -13,11 +13,11 @@ plot_violin <- function(.data, .x, .ncol = NULL) {
     guides(colour = 'none')
 }
 
-plot_linedot <- function(.data, .x, .ncol = NULL) {
+plot_linedot <- function(.data, .x, .ncol = NULL, xint = 0.95) {
   ggplot(data = .data, aes(x = {{.x}}, y = fit_family, colour = fit_family)) +
   geom_linerange(xmin = 0, mapping = aes(xmax = {{.x}})) +
   geom_point(size = 3) +
-  geom_vline(xintercept = 0.95, linetype = 'dashed') +
+  geom_vline(xintercept = xint, linetype = 'dashed') +
   scale_color_brewer(palette = "Dark2") +
   facet_wrap(~ title, ncol = .ncol) +
   guides(colour = 'none')
@@ -31,8 +31,17 @@ ind_dir <- here::here('data-outputs', 'index')
 # ------------------------------------------------------------------------------
 Q_values <- dget(file.path('data-outputs', 'Q-values.txt'))
 
+relevel_fit_family <- function(df, fam_levels = c('gengamma', 'gamma', 'tweedie', 'lognormal' )) {
+  df |>
+  mutate(fit_family = tolower(fit_family)) |>
+  mutate(fit_family = factor(fit_family, fam_levels))
+}
+
+# Look at real data to figure out what these should be.
+Qs_to_plot <- c(-1, 0.001, 0.5, 0.8, 1, 2)
+
 index_df <- file.path(ind_dir, list.files(ind_dir)) |>
-  purrr::map_dfr(readRDS)
+  purrr::map_dfr(readRDS) |>
 
 cross_combos <- bind_rows(
   tibble(Q = NA, sim_family = c('delta-lognormal', 'delta-gamma', 'tweedie')),
@@ -63,7 +72,8 @@ title_levels <- c(title_levels[-1], title_levels[1])
 plot_df <- plot_df |>
   mutate(title = factor(title, levels = title_levels)) |>
   filter(!(Q %in% c(-5, -0.001, 5))) |>
-  mutate(xtitle = paste0('cv = ', cv, "\nsigma_O = ", sigma_O))
+  mutate(xtitle = paste0('cv = ', cv, "\nsigma_O = ", sigma_O)) |>
+  relevel_fit_family()
 
 
 # - Compare: RMSE, MRE, coverage, AIC
@@ -123,7 +133,7 @@ sanity_tally |>
   mutate(title = ifelse(is.na(Q), sim_family, paste0(sim_family, ": Q=", signif(Q, digits = 2)))) |>
   mutate(title = factor(title, levels = title_levels)) |>
   mutate(xtitle = paste0('cv = ', cv, "\nsigma_O = ", sigma_O)) |>
-plot_linedot(.x = pass_prop, .ncol = 5) +
+plot_linedot(.x = pass_prop, .ncol = 5, xint = 1.0) +
   facet_grid(xtitle ~ title) +
   ggtitle("Passed sanity check")
 ggsave(filename = file.path(fig_dir, paste0('sanity-pass', tag, '.png')), width = 11, height = 6.5)
@@ -141,7 +151,20 @@ fit_df |>
 
 mean_ests <- fit_df |>
   group_by(sim_family, Q, fit_family) |>
-  summarise_at(vars(est_Q:std.error_tweedie_p), mean, groups = ".drop")
+  summarise_at(vars(est_Q:std.error_tweedie_p), mean, na.rm = TRUE, groups = ".drop")
 
-test <- left_join(cross_combos, mean_ests) |>
-  tidyr::pivot_longer(col)
+#fit_df |>
+mean_ests |>
+  filter(sim_family == 'delta-gengamma', fit_family == 'gengamma') |>
+  select(sim_family:est_Qse) |>
+ggplot(aes(x = Q, y = est_Q)) +
+  geom_point(aes(y = est_Q + 1.96 * est_Qse), stroke = 3, size = 6, shape = "-", colour = '#2c7bb6', alpha = 1) +
+  geom_point(aes(y = est_Q - 1.96 * est_Qse), stroke = 3, size = 6, shape = "-", colour = '#D7191C', alpha = 1) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1) +
+  labs(x = "Simulation Q", y = "Estimated Q")
+ggsave(filename = file.path(fig_dir, paste0('Q-estimation', tag, '.png')), width = 4.5, height = 4)
+# Notes:
+# - cannot recover Q < -1 very well (I'm assuming because there is just too little
+# information at the tails)
+# - similarly cannot seem to recover Q at Q = 5
