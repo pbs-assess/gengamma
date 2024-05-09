165c46099af014390fd6663f9c9c5225106945be
Jillian Dunic <>
2024-05-09 15:21:45 -0700

    styling

--- R/multiple-stocks.R
+++ R/multiple-stocks.R
@@ -1,6 +1,6 @@
 library(dplyr)
-#devtools::load_all('../sdmTMB')
-#pak::pkg_install("pbs-assess/sdmTMB")
+# devtools::load_all('../sdmTMB')
+# pak::pkg_install("pbs-assess/sdmTMB")
 library(sdmTMB)
 library(ggplot2)
 theme_set(gfplot::theme_pbs())
@@ -11,14 +11,14 @@ source("R/00-utils.R")
 dat <- readRDS(file.path("data-outputs", "clean-survey-data.rds")) |>
   filter(survey_abbrev != "SYN WCHG")
 spp_regions <- distinct(dat, species, survey_abbrev) |>
-  rename(.species = 'species', .region = 'survey_abbrev')
+  rename(.species = "species", .region = "survey_abbrev")
 
 # Simplified synoptic grid
 syn_grid <- as_tibble(gfplot::synoptic_grid) |>
   select(survey, X, Y, depth)
 
-#tag <- '_sp-on-st-iid'
-#tag <- '_sp-on-st-off'
+# tag <- "_sp-on-st-iid"
+# tag <- "_sp-on-st-off"
 .spatial <- "off"
 .spatiotemporal <- "off"
 tag <- paste0("_sp-", .spatial, "-st-", .spatiotemporal, "", sep = "")
@@ -38,16 +38,16 @@ dir.create(fit_dir, showWarnings = FALSE, recursive = TRUE)
 #   right_join(spp_regions) |>
 #   mutate(.id = row_number())
 
-#species <- spp_list[[4]]
-#species <- 'north pacific spiny dogfish'
-#scaler <- 1e-2 # works
-#region <- 'SYN WCVI'
+# species <- spp_list[[4]]
+# species <- "north pacific spiny dogfish"
+# scaler <- 1e-2 # works
+# region <- "SYN WCVI"
 
-# species <- 'walleye pollock'
-# region <- 'SYN WCVI'
+# species <- "walleye pollock"
+# region <- "SYN WCVI"
 # scaler <- 1e-3 # QCS needs 1e-1; nothing seems to fix SYN WCVI
 
-#sp_file <- paste0(clean_name(species), ".rds")
+# sp_file <- paste0(clean_name(species), ".rds")
 # cutoff <- 8
 # save_fits <- TRUE
 
@@ -75,11 +75,11 @@ dir.create(fit_dir, showWarnings = FALSE, recursive = TRUE)
 #     error = function(e) paste(species, region, family, "\n\tError:", e, sep = " - ")
 #   )
 
-#   if (inherits(fit, 'sdmTMB')) {
+#   if (inherits(fit, "sdmTMB")) {
 #     sanity_check <- all(unlist(sdmTMB::sanity(fit, gradient_thresh = 0.005)))
 #   }
 #   # Turn off spatial field if model does not fit and spatiotemporal == "off"
-#   if ((!inherits(fit, 'sdmTMB') | !sanity_check) & (sp == "on" & st == "off")) {
+#   if ((!inherits(fit, "sdmTMB") | !sanity_check) & (sp == "on" & st == "off")) {
 #     message("\tFitting: sp = ", sp, ", st = ", st, " for ", species, "-", region, "-", family, " failed")
 #     message("\tUpdating with sp = off")
 #     fit <- tryCatch(
@@ -91,18 +91,19 @@ dir.create(fit_dir, showWarnings = FALSE, recursive = TRUE)
 # }
 
 regions <- c("SYN WCVI", "SYN QCS", "SYN HS", "SYN WCHG")
-families <- c("tweedie",
-  "delta-gamma", "delta-lognormal", "delta-gengamma")
-  #"delta-plink-lognormal", "delta-plink-gamma", "delta-plink-gengamma")
-tofit <- tidyr::expand_grid(.region = regions, .family = families,
-  .species = spp_list) |>
+families <- c("tweedie", "delta-gamma", "delta-lognormal", "delta-gengamma")
+# "delta-plink-lognormal", "delta-plink-gamma", "delta-plink-gengamma")
+tofit <- tidyr::expand_grid(
+  .region = regions, .family = families,
+  .species = spp_list
+) |>
   mutate(.id = row_number())
 
 # test_fit <-
 #   tofit |>
 #     filter(.species == species) |>
 #     filter(.region == region) |>
-#     filter(.family == 'delta-gengamma') |>
+#     filter(.family == "delta-gengamma") |>
 #     purrr::pmap(\(.region, .family, .id, .species) {
 #         get_fit(survey_dat = dat |> mutate(catch_weight = catch_weight * scaler),
 #             formula = as.formula(catch_weight ~ 0 + as.factor(year)),
@@ -122,51 +123,58 @@ tofit <- tidyr::expand_grid(.region = regions, .family = families,
 survey_dat <- dat
 cutoff <- 8
 regions <- c("SYN WCVI", "SYN QCS", "SYN HS")
-families <- c("tweedie",
-  "delta-gamma", "delta-lognormal", "delta-gengamma")
-  #"delta-plink-lognormal", "delta-plink-gamma", "delta-plink-gengamma")
+families <- c(
+  "tweedie",
+  "delta-gamma", "delta-lognormal", "delta-gengamma"
+)
+# "delta-plink-lognormal", "delta-plink-gamma", "delta-plink-gengamma")
 family_lu <- c(
   "tweedie" = "tweedie",
   "delta-gamma" = "Gamma",
   "delta-lognormal" = "lognormal",
-  "delta-gengamma" = "gengamma") %>%
+  "delta-gengamma" = "gengamma"
+) %>%
   tibble(.family = names(x = .), fit_family = .)
-tofit <- tidyr::expand_grid(.region = regions, .family = families,
-  .species = unique(spp_regions$.species)) |>
+tofit <- tidyr::expand_grid(
+  .region = regions, .family = families,
+  .species = unique(spp_regions$.species)
+) |>
   mutate(.id = row_number())
 
 lu_df <- tofit |>
   left_join(family_lu) |>
   mutate(sp_hyp = clean_name(.species)) |>
   tidyr::unite(col = "fname", sp_hyp, .region, .family, sep = "-", remove = FALSE)
-saveRDS(lu_df, file.path(out_dir, 'lu-df.rds'))
+saveRDS(lu_df, file.path(out_dir, "lu-df.rds"))
 # START WITH st = "off" for now.... in part because I want to better understand:
 # https://github.com/pbs-assess/sdmTMB/issues/263
 # it is also a lot faster
 # Fit models
-#overwrite_cache = TRUE
-overwrite_cache = FALSE
-#choose_multi_type(cores = 8)
+# overwrite_cache = TRUE
+overwrite_cache <- FALSE
+# choose_multi_type(cores = 8)
 future::plan(future::multisession, workers = 10)
 progressr::with_progress({
   handler <- progressr::progressor(along = tofit$.id)
   fits <- tofit |>
-    #purrr::pmap(\(.region, .family, .id, .species) {
+    # purrr::pmap(\(.region, .family, .id, .species) {
     furrr::future_pmap(\(.region, .family, .id, .species) {
       handler() # Signal progress
       sp_file <- file.path(fit_dir, paste0(clean_name(.species), "-", .region, "-", .family, ".rds"))
       if (overwrite_cache | !file.exists(sp_file)) {
         message("Fitting: ", sp_file)
-        f <- get_fit(survey_dat = survey_dat,
-                formula = as.formula(catch_weight ~ 0 + as.factor(year)),
-                offset = "offset",
-                region = .region, family = .family, species = .species,
-                cutoff = cutoff, sp = .spatial, st = .spatiotemporal,
-                use_priors = FALSE)
+        f <- get_fit(
+          survey_dat = survey_dat,
+          formula = as.formula(catch_weight ~ 0 + as.factor(year)),
+          offset = "offset",
+          region = .region, family = .family, species = .species,
+          cutoff = cutoff, sp = .spatial, st = .spatiotemporal,
+          use_priors = FALSE
+        )
         message("Saving fits: ", sp_file)
         saveRDS(f, sp_file)
       }
-  })
+    })
   beep()
   future::plan(future::sequential())
 })
@@ -175,26 +183,27 @@ progressr::with_progress({
 # Examine outputs
 # ------------------------------------------------------------------------------
 f_name <- c(
-  list.files(file.path(out_dir, 'fits_sp-off-st-off')),
-  list.files(file.path(out_dir, 'fits_sp-on-st-off')),
-  list.files(file.path(out_dir, 'fits_sp-on-st-on'))
+  list.files(file.path(out_dir, "fits_sp-off-st-off")),
+  list.files(file.path(out_dir, "fits_sp-on-st-off")),
+  list.files(file.path(out_dir, "fits_sp-on-st-on"))
 )
-f_name <- f_name[!grepl('WCHG', f_name)]
+f_name <- f_name[!grepl("WCHG", f_name)]
 f <- file.path(fit_dir, f_name)
 
-fits <-lapply(f, readRDS) |>
+fits <- lapply(f, readRDS) |>
   setNames(stringr::str_extract(f_name, ("(.*)\\.rds"), group = 1))
 beep()
 
-#tag
+# tag
 tag <- "-random-effects-on-and-off"
 sanity_filename <- paste0("sanity-df", tag, ".rds")
 if (!file.exists(file.path(out_dir, sanity_filename))) {
-sanity_df <- purrr::keep(fits, ~inherits(.x, "sdmTMB")) |>
-  purrr::map_dfr(get_sanity_df, real_data = TRUE, silent = TRUE, .gradient_thresh = 0.005) |>
-  left_join(lu_df, by = c("species" = ".species", "region" = ".region", "fit_family"))
-saveRDS(sanity_df, file.path(out_dir, sanity_filename))
-} else {message("\t Loading cached sanity-df")
+  sanity_df <- purrr::keep(fits, ~ inherits(.x, "sdmTMB")) |>
+    purrr::map_dfr(get_sanity_df, real_data = TRUE, silent = TRUE, .gradient_thresh = 0.005) |>
+    left_join(lu_df, by = c("species" = ".species", "region" = ".region", "fit_family"))
+  saveRDS(sanity_df, file.path(out_dir, sanity_filename))
+} else {
+  message("\t Loading cached sanity-df")
   sanity_df <- readRDS(file.path(out_dir, sanity_filename))
 }
 
@@ -208,9 +217,10 @@ predictions <- purrr::map(ok_fits, \(x) {
   years <- unique(x$data$year)
   sg <- syn_grid |> filter(survey %in% region)
   nd <- sdmTMB::replicate_df(
-      dat = sg,
-      time_name = "year",
-      time_values = years)
+    dat = sg,
+    time_name = "year",
+    time_values = years
+  )
   predict(x, newdata = nd, return_tmb_object = TRUE)
 })
 
@@ -219,12 +229,12 @@ inds3 <- purrr::map(predictions[301:472], get_index, bias_correct = TRUE) |>
   purrr::map2(.x = _, .y = names(predictions[301:472]), ~ mutate(.x, fname = .y)) |>
   bind_rows() #|>
 tictoc::toc()
-  left_join(lu_df) |>
+left_join(lu_df) |>
   select(-fname)
 
 index_filename <- paste0("index-df", tag, ".rds")
 bind_rows(inds, inds2, inds3) |>
-saveRDS(file.path(out_dir, index_filename))
+  saveRDS(file.path(out_dir, index_filename))
 
 fit_est_filename <- paste0("fitted-estimates", tag, ".rds")
 fit_ests <- purrr::map(fits, get_fitted_estimates, real_data = TRUE) |>
@@ -239,11 +249,12 @@ saveRDS(fit_ests, file.path(out_dir, fit_est_filename))
 
 test <- left_join(fit_ests, sanity_df)
 
-test |> filter(all_ok == FALSE) |>
+test |>
+  filter(all_ok == FALSE) |>
   View()
 
-# gg_fits <- ok_fits[grepl('gengamma', names(ok_fits))] |>
-#   purrr::keep(~ inherits(.x, 'sdmTMB'))
+# gg_fits <- ok_fits[grepl("gengamma", names(ok_fits))] |>
+#   purrr::keep(~ inherits(.x, "sdmTMB"))
 # gg_fit_ests <- gg_fits|>
 #   purrr::map(get_fitted_estimates, real_data = TRUE) |>
 #   purrr::map2(.x = _, .y = names(gg_fits), ~ mutate(.x, fname = .y)) |>
@@ -255,27 +266,28 @@ test |> filter(all_ok == FALSE) |>
 
 gg_fit_ests
 
-rqr_residuals1 <- purrr::map2_dfr(ok_fits[1:30], names(ok_fits[1:30]), ~get_rqr(.x, id = .y))
-rqr_residuals2 <- purrr::map2_dfr(ok_fits[31:100], names(ok_fits[31:100]), ~get_rqr(.x, id = .y))
+rqr_residuals1 <- purrr::map2_dfr(ok_fits[1:30], names(ok_fits[1:30]), ~ get_rqr(.x, id = .y))
+rqr_residuals2 <- purrr::map2_dfr(ok_fits[31:100], names(ok_fits[31:100]), ~ get_rqr(.x, id = .y))
 
-rqr_residuals3 <- purrr::map2_dfr(ok_fits[101:length(ok_fits)], names(ok_fits[101:length(ok_fits)]),
+rqr_residuals3 <- purrr::map2_dfr(
+  ok_fits[101:length(ok_fits)], names(ok_fits[101:length(ok_fits)]),
   ~ {
-  message(.y)
-  get_rqr(.x, id = .y)
+    message(.y)
+    get_rqr(.x, id = .y)
   }
-  )
+)
 
 
 rqr_residuals <- bind_rows(rqr_residuals1, rqr_residuals2, rqr_residuals3)
 
-test <- residuals(ok_fits[[5]], type = 'mle-mvn', model = 2)
+test <- residuals(ok_fits[[5]], type = "mle-mvn", model = 2)
 str(test)
 
 # Examine single species
 sp_f <- f[grepl("north-pacific-spiny-dogfish", f)]
 fa <- lapply(sp_f, readRDS) |>
   setNames(stringr::str_extract(sp_f, (".*fits/(.*)\\.rds"), group = 1))
-sp_fits <- purrr::keep(fa, ~inherits(.x, "sdmTMB"))
+sp_fits <- purrr::keep(fa, ~ inherits(.x, "sdmTMB"))
 test <- sp_fits |>
   purrr::map_dfr(get_sanity_df, real_data = TRUE, .gradient_thresh = 0.005)
 
@@ -289,16 +301,18 @@ rqr_df <- purrr::map_dfr(1:length(sp_fits), ~ get_rqr(sp_fits[[.x]], id = names(
 rqr_df |>
   mutate(region = stringr::str_extract_all(id, "(.*)-(SYN [A-Z]{2,4})-(.*)"))
 
-rqr_df  |>
-   mutate(species = stringr::str_extract(id, ".*(?=-SYN)"),
-          region = stringr::str_extract(id, ".*-(SYN [A-Z]{2,4})-.*", group = 1),
-          family = stringr::str_extract(id, ".*-SYN [A-Z]{2,4}-(.*)", group = 1)) |>
-ggplot(aes(sample = r)) +
+rqr_df |>
+  mutate(
+    species = stringr::str_extract(id, ".*(?=-SYN)"),
+    region = stringr::str_extract(id, ".*-(SYN [A-Z]{2,4})-.*", group = 1),
+    family = stringr::str_extract(id, ".*-SYN [A-Z]{2,4}-(.*)", group = 1)
+  ) |>
+  ggplot(aes(sample = r)) +
   geom_qq() +
   geom_abline(intercept = 0, slope = 1) +
   facet_grid(region ~ family)
-  #facet_wrap(~ family)
-ggsave(filename = file.path('figures', 'bc-gf-data', 'exploratory', 'dogfish-rqr.png'))
+# facet_wrap(~ family)
+ggsave(filename = file.path("figures", "bc-gf-data", "exploratory", "dogfish-rqr.png"))
 
 
 # Splitting the capture groups into separate columns
