601af7a1ce59bee1e86cace5265c3c7bb61dc931
Sean Anderson <sean@seananderson.ca>
2025-02-10 18:14:22 -0800

    Setup cross validation

--- R/03-multi-stock-cross-validation.R
+++ R/03-multi-stock-cross-validation.R
@@ -62,39 +62,55 @@ library(future)
 # pluck(docv, "spatial_list", 1) |> as.list()
 
 survey_dat <- readRDS(here::here("data-outputs", "data-used.rds"))
+survey_dat <- readRDS("~/Downloads/data-used.rds")
+survey_dat <- filter(survey_dat, !(region == "GOA" & species == "petrale sole"))
+unique(survey_dat$species)
+unique(survey_dat$region)
 
-dat <- survey_dat |> filter(species == "arrowtooth flounder", region == "GOA")
-mesh <- sdmTMB::make_mesh(dat, xy_cols = c("X", "Y"), type = "cutoff_search", n_knots = 500)
+select(survey_dat, species, region) |>
+  distinct() |> as.data.frame()
+
+# dat <- survey_dat |> filter(species == "arrowtooth flounder", region == "GOA")
+# plot(mesh)
 # .spatial <-
 # .spatiotemporal <- docv |> slice(1) |> purrr::pull(".spatiotemporal")
-.family <- "delta-gamma"
 
-tictoc::tic()
+fit_all <- function(.dat, .family) {
+  set.seed(1199328)
+  if (unique(.dat$region) == "GOA") {
+    mesh <- sdmTMB::make_mesh(dat, xy_cols = c("X", "Y"), type = "cutoff_search", n_knots = 500)
+  } else {
+    mesh <- sdmTMB::make_mesh(dat, xy_cols = c("X", "Y"), cutoff = 8)
+  }
+  fit <- tryCatch({sdmTMB_cv(
+    formula = as.formula(catch_weight ~ 0 + as.factor(year)),
+    data = .dat,
+    mesh = mesh,
+    time = "year",
+    spatial = "on",
+    spatiotemporal = "iid",
+    offset = "offset",
+    family = choose_family(.family),
+    control = sdmTMBcontrol(multiphase = FALSE, profile = TRUE),
+    k_folds = 25
+  )}, error = function(e) NA)
+  if (length(fit) > 1) {
+    data.frame(family = .family, region = unique(.dat$region), species = unique(.dat$species), loglik = fit$sum_loglik)
+  } else {
+    data.frame(family = .family, region = unique(.dat$region), species = unique(.dat$species), loglik = NA_real_)
+  }
+}
+
+dsplit <- group_by(survey_dat, species, region) |> group_split()
 plan(multisession, workers = 6)
-test_gamma <- sdmTMB_cv(
-  formula = as.formula(catch_weight ~ 0 + as.factor(year)),
-  data = dat,
-  mesh = mesh,
-  time = "year",
-  spatial = list("on", "on"),
-  spatiotemporal = list("iid", "iid"),
-  offset = "offset",
-  family = choose_family(.family),
-  k_folds = 12
-)
-tictoc::toc()
-# beepr::beep()
+df_lg <- purrr::map_dfr(dsplit, fit_all, .family = "delta-lognormal")
+df_dg <- purrr::map_dfr(dsplit, fit_all, .family = "delta-gamma")
+df_tw <- purrr::map_dfr(dsplit, fit_all, .family = "tweedie")
+df_dgg <- purrr::map_dfr(dsplit, fit_all, .family = "delta-gengamma")
 future::plan(future::sequential)
 
+saveRDS(df_lg, file = here::here("data-outputs", "cross-val-lognormal.rds"))
+saveRDS(df_dg, file = here::here("data-outputs", "cross-val-gamma.rds"))
+saveRDS(df_tw, file = here::here("data-outputs", "cross-val-tweedie.rds"))
+saveRDS(df_dgg, file = here::here("data-outputs", "cross-val-gengamma.rds"))
 
-test_gamma$models[[1]] |> sanity()
-test_gamma$models[[2]] |> sanity()
-test_gamma$models[[3]] |> sanity()
-test_gamma$models[[4]] |> sanity()
-test_gamma$models[[5]] |> sanity()
-test_gamma$models[[6]] |> sanity()
-test_gamma$models[[7]] |> sanity()
-test_gamma$models[[8]] |> sanity()
-test_gamma$models[[9]] |> sanity()
-test_gamma$models[[10]] |> sanity()
-test_gamma$models[[11]] |> sanity()
